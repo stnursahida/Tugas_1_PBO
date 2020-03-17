f60114cabc4f60ac7c49639a7b6ea63f733d7ee7
stnursahida <57305756+stnursahida@users.noreply.github.com>
2020-03-17 13:22:33 +0800

    Add files via upload

---
+++ nomor2.java
@@ -0,0 +1,6 @@
+public class nomor2{
+	public static void main(String[] args)
+	{
+	System.out.println("Belajar Bahasa Pemrograman Java");
+	}
+}
---
+++ nomor3.java
@@ -0,0 +1,6 @@
+public class nomor3{
+	public static void main(String[] args)
+	{
+		System.out.println("selesai belajar pemrograman java");
+	}
+}
